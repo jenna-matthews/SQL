571e1cd45079f454dad68b653573fbee62cdb309
Jenna Olsen <jennaanneolsen@gmail.com>
2019-06-26 07:35:46 -0600

    Update useful_bits_postgresql.sql

--- useful_bits_postgresql.sql
+++ useful_bits_postgresql.sql
@@ -8,3 +8,15 @@ from information_schema.tables
 	)y
 where row_count > 0
 order by row_count desc
+
+--find tables/columns with name like ...
+select t.table_schema,
+       t.table_name, c."column_name"
+from information_schema.tables t
+inner join information_schema.columns c on c.table_name = t.table_name 
+                                and c.table_schema = t.table_schema
+where c.column_name like '%pk1%'
+      and t.table_schema not in ('information_schema', 'pg_catalog')
+      and t.table_type = 'BASE TABLE'
+      and t."table_name" like '%resou%'
+order by t."table_name";
