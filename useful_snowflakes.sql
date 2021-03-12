1f675e7f12949d94df438e241192ec905479ce1c
Jenna Olsen <jennaanneolsen@gmail.com>
2021-03-12 07:05:09 -0700

    Update useful_snowflakes.sql

--- useful_snowflakes.sql
+++ useful_snowflakes.sql
@@ -4,3 +4,7 @@ from x.INFORMATION_SCHEMA.COLUMNS c
     join x.INFORMATION_SCHEMA.TABLES t on c.TABLE_NAME = t.TABLE_NAME and c.TABLE_SCHEMA = t.TABLE_SCHEMA
 where COLUMN_NAME like '%CLUST%'
 and t.ROW_COUNT > 0;
+
+--change default role for the server
+--avoids the nuisance of switching, plus it's useful for connecting other applications like Tableau
+alter user SET DEFAULT_ROLE = (name of the role that should be the default)
